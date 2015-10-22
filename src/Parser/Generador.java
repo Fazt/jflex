b7f31ceb3849a126e5005486a1d2c8be4b630560
Spartan <dsniper007@gmail.com>
2015-10-22 17:48:46 -0300

    Archivos para el Parser

---
+++ src/Parser/Generador.java
@@ -0,0 +1,24 @@
+package Parser;
+import java.io.File;
+import java.io.IOException;
+import java_cup.*;
+public class Generador {
+
+    public static void main(String[] args) throws internal_error, IOException, Exception {
+    	String[] argv = new String[7];
+    	//Directorio destino: "src/parser"
+    	argv[0] = "-destdir";
+    	argv[1] = "src"+File.separator+"parser";
+    	//Nombre del paquete: "Parser"
+    	argv[2] = "-package";
+    	argv[3] = "Analizador";
+    	//Nombre de la clase: "Parser"
+    	argv[4] = "-parser";
+    	argv[5] = "Analizer";
+    	//Archivo fuente.
+    	argv[6] = "src"+File.separator+"parser"+ File.separator + "Parser.cup";
+    	//Generar
+    	java_cup.Main.main(argv);
+    }
+    
+}
